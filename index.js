c944b31e92b605e31a72fbd605222ce208f79adb
Navid Hasan <navid.nyon@gmail.com>
2025-05-11 02:41:19 +0600

    initially ready for submission and all done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const { ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -29,12 +30,73 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
 
-    // blogs related apis
+    // api collections
     const blogsCollection = client.db("blogsDB").collection("blogs");
+    const wishlistCollection = client.db("blogsDB").collection("wishlist");
+    const commentsCollection = client.db("blogsDB").collection("comments");
+
+    const currentDate = new Date().toISOString();
+    // wishlist related apis
+    app.post("/wishlist", async (req, res) => {
+      const myWishlist = req.body;
+
+      const query = { email: myWishlist.email, blogID: myWishlist.blogID };
+      const alreadyExist = await wishlistCollection.findOne(query);
+      if (alreadyExist)
+        return res.status(400).send("Already exists in your wishlist");
+      const result = await wishlistCollection.insertOne(myWishlist);
+      res.send(result);
+    });
+
+    app.get("/wishlist", async (req, res) => {
+      const result = await wishlistCollection.find().toArray();
+      res.send(result);
+    });
+
+    // delete entries
+    app.delete("/wishlist/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await wishlistCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // all blogs
     app.get("/blogs", async (req, res) => {
       const result = await blogsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/blogs6", async (req, res) => {
+      const query = blogsCollection.find().sort({ createdAt: -1 }).limit(6);
+      const result = await query.toArray();
+      res.send(result);
+    });
+
+    // update blog api
+    app.patch("/update/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const data = req.body;
+
+      const update = {
+        $set: {
+          title: data.title,
+          category: data.category,
+          image: data.image,
+          shortDescription: data.shortDescription,
+          longDescription: data.longDescription,
+        },
+      };
+      const result = await blogsCollection.updateOne(query, update);
+      res.send(result);
+    });
+
+    // blog details api
+    app.get("/blog/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await blogsCollection.findOne(query);
       res.send(result);
     });
 
