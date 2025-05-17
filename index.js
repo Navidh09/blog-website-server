7853694df62ea6b9a79c9f067e1884eaaa70da72
Navid Hasan <navid.nyon@gmail.com>
2025-05-17 20:20:49 +0600

    all done

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7n3bd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
